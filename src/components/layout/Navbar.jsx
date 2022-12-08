3f381209f4831bccfb2b84559d8fd89345799e19
DOriginalSamxie <samadrufai@gmail.com>
2022-12-08 16:34:10 +0100

    navbar flex

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -6,8 +6,8 @@ function Navbar({ title }) {
   return (
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
       <div className='container mx-auto'>
-        <div className=' px-2 mx-2'>
-          <FaGithub className=' pr-2 text-3xl' />
+        <div className='flex-none px-2 mx-2'>
+          <FaGithub className='inline pr-2 text-3xl' />
           <Link to='/' className='text-lg font-bold align-middle'>
             {title}
           </Link>
